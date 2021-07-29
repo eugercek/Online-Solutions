628b77a33285c7f1f3a9169ff7fd5169f079291f
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-07-29 20:26:21 +0300

    exercism: Change macro in CL/rna-transcription

--- exercism/common-lisp/rna-transcription/rna-transcription.lisp
+++ exercism/common-lisp/rna-transcription/rna-transcription.lisp
@@ -7,15 +7,18 @@
 (defmacro comp (nuc &body body)
   (let ((pairs
           (loop for (a b) in body
-                collect `((char= ,nuc ,a) ,b))))
-    `(cond ,@pairs
-           (t 'error))))
+                collect `(,a ,b))))
+    `(case ,nuc
+       ,@pairs
+       (t 'error))))
+
 (defun nuc-comp (nuc)
   (comp nuc
-        (#\G #\C)
-        (#\C #\G)
-        (#\T #\A)
-        (#\A #\U)))
+    (#\G #\C)
+    (#\C #\G)
+    (#\T #\A)
+    (#\A #\U)))
+
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
   (map 'string #'nuc-comp (string str))) ; Extra (string) is for char inputs
