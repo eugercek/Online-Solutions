b8be694ed879eb570258b171052c6de924c034ce
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-07-30 04:05:09 +0300

    exercism: Add CL/rna-transcription simple solution
    My mentor told me about alist and assoc.
    They are useful.

--- exercism/common-lisp/rna-transcription/rna-transcription.lisp
+++ exercism/common-lisp/rna-transcription/rna-transcription.lisp
@@ -4,6 +4,27 @@
   (:export #:to-rna))
 (in-package #:rna-transcription)
 
+;; Simpler solution
+;; (defvar nucleotid-map                   ; For green guys
+;;     '((#\G . #\C)
+;;       (#\C . #\G)
+;;       (#\T . #\A)
+;;       (#\A . #\U)))
+
+;; (defun nuc-comp (char)
+;;   (cdr (assoc char nucleotid-map)))
+
+;; (defun to-rna (str)
+;;   "Transcribe a string representing DNA nucleotides to RNA."
+;;   (map 'string #'nuc-comp (string str))) ; Extra (string) is for char inputs
+
+;; (defmacro comp (nuc &body body)
+;;   (let ((pairs
+;;           (loop for (a b) in body
+;;                 collect `(,a ,b))))
+;;     `(ecase ,nuc
+;;        ,@pairs)))
+
 (defmacro comp (nuc &body body)
   (let ((pairs
           (loop for (a b) in body
