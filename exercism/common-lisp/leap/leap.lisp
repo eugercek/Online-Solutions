649ef1862a472fc877676a78f652daf66b442cba
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-01 01:04:02 +0300

    exercism: Add CL/leap macro version

--- exercism/common-lisp/leap/leap.lisp
+++ exercism/common-lisp/leap/leap.lisp
@@ -3,9 +3,33 @@
   (:export #:leap-year-p))
 (in-package #:leap)
 
+;; Simple solution
+
+;; (defun leap-year-p (year)
+;;   (defun divisiblep (num)
+;;     (zerop (mod year num)))
+;;   (cond ((divisiblep 400) t)
+;;         ((divisiblep 100) nil)
+;;         ((divisiblep 4) t)))
+
+(defun list-to-pairs (l)
+  "'(1 2 3 4) -> '((1 2) (3 4))"
+  (loop for (a b) on l
+        :by #'cddr
+        collect (cons a b)))
+
+(defun divisible-p (num base)           ; Can (NUM YEAR)
+  (zerop (mod base num)))
+
+(defmacro div-case (year &body body)
+  "Return the number num where,year (mod num) = 0"
+  (let ((parsed (list-to-pairs body)))
+    `(cond
+       ,@(loop for (num . res) in parsed
+               collect `((divisible-p ,num ,year) ,res)))))
+
 (defun leap-year-p (year)
-  (defun divisiblep (num)
-    (zerop (mod year num)))
-  (cond ((divisiblep 400) t)
-        ((divisiblep 100) nil)
-        ((divisiblep 4) t)))
+  (div-case year
+    400 t
+    100 nil
+    4 t))
